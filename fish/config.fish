1208e315c75a2efad2b0b88d5dc3681fe50d436c
Ian Will <icwill@gmail.com>
2017-12-14 10:20:42 -0500

    Remove work specific stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 
 source ~/.config/fish/functions/fish_command_timer.fish
 set fish_command_timer_color grey
-set -x VAULT_ADDR "https://prod-vault-lb.aws.opower.it:8200"
 
 if status --is-login
     set -x PATH ./node_modules/.bin ~/bin /opt/chefdk/bin $PATH
