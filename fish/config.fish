9094224d97404447760ac2987cf55ff0d7327f41
Ian Will <icwill@gmail.com>
2017-12-14 10:16:31 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,18 @@
-source ~/.asdf/asdf.fish
 
 source ~/.config/fish/functions/fish_command_timer.fish
 set fish_command_timer_color grey
 set -x VAULT_ADDR "https://prod-vault-lb.aws.opower.it:8200"
-set -U fish_user_paths ~/bin ./node_modules/.bin $fish_user_paths
-set -U EDITOR nvim
 
-function unset_proxy
-  set -e http_proxy;
-  set -e https_proxy;
-  set -e all_proxy;
-  set -e no_proxy;
+if status --is-login
+    set -x PATH ./node_modules/.bin ~/bin /opt/chefdk/bin $PATH
 end
 
-function setup_proxy
-  set -x http_proxy http://www-proxy-adcq7-new.us.oracle.com:80
-  set -x https_proxy http://www-proxy-adcq7-new.us.oracle.com:80
-  set -x all_proxy http://www-proxy-adcq7-new.us.oracle.com:80
-  set -x no_proxy "127.0.0.1,localhost,chef-2001.ca.opower.it,opower.it,.hexxie.com,.xip.io"
-end
-
+set -x EDITOR nvim
 # Alias vim to nvim
 function vim
     nvim $argv
+end
+
+function vi
+    vim $argv
 end
